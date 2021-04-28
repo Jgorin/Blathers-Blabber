45d94128bd2f0f7e30c5d0053a1703df446183b8
Michael Catanese <michaelcatanese@Catanese-Family-Computer.local>
2021-04-28 14:39:14 -0400

    fixed testing

--- spec/features/controllers/api/v1/animals_controller_spec.rb
+++ spec/features/controllers/api/v1/animals_controller_spec.rb
@@ -5,13 +5,11 @@ RSpec.describe Api::V1::AnimalsController, type: :controller do
     name: "hippo",
     body: "fat",
     rating: 5.0,
-    photo_path: "./spec/support/images/photo.png"
   ) }
   let!(:second_animal) { Animal.create(
     name: "penguin",
     body: "frosty",
     rating: 4.8,
-    photo_path: "./spec/support/images/photo.png"
   ) }
 
   let!(:user1) {User.create(
@@ -36,15 +34,14 @@ RSpec.describe Api::V1::AnimalsController, type: :controller do
       expect(response.status).to eq 200
       expect(response.content_type).to eq("application/json")
 
+      expect(returned_json["animals"][0]["name"]).to eq "hippo"
+      expect(returned_json["animals"][0]["body"]).to eq "fat"
+      expect(returned_json["animals"][0]["rating"]).to eq 5.0
 
-      expect(returned_json[0]["name"]).to eq "hippo"
-      expect(returned_json[0]["body"]).to eq "fat"
-      expect(returned_json[0]["rating"]).to eq 5.0
-
-      expect(returned_json.length).to eq 2
-      expect(returned_json[1]["name"]).to eq "penguin"
-      expect(returned_json[1]["body"]).to eq "frosty"
-      expect(returned_json[1]["rating"]).to eq 4.8
+      expect(returned_json["animals"].length).to eq 2
+      expect(returned_json["animals"][1]["name"]).to eq "penguin"
+      expect(returned_json["animals"][1]["body"]).to eq "frosty"
+      expect(returned_json["animals"][1]["rating"]).to eq 4.8
 
     end
   end
@@ -59,7 +56,7 @@ RSpec.describe Api::V1::AnimalsController, type: :controller do
       expect(returned_json["animal"]["name"]).to eq first_animal.name
       expect(returned_json["animal"]["body"]).to eq first_animal.body
       expect(returned_json["animal"]["rating"]).to eq first_animal.rating
-      expect(returned_json["reviews"][0]["description"]).to eq review.description
+      expect(returned_json["animal"]["reviews"][0]["description"]).to eq review.description
     end
   end
 end
