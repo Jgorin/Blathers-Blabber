98dc1c6f94eea53d25d31843c31c80c3e3d5c499
Tony Nguyen <tonynguyen3645@gmail.com>
2021-04-29 12:04:57 -0400

    Fixed new_animal_page rspec tests

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -12,7 +12,9 @@ require 'capybara/rspec'
 # Add additional requires below this line. Rails is not loaded until this point!
 RSpec.configure do |config|
   config.include Devise::Test::ControllerHelpers, type: :controller
+  config.include Warden::Test::Helpers
 end
+
 # Requires supporting ruby files with custom matchers and macros, etc, in
 # spec/support/ and its subdirectories. Files matching `spec/**/*_spec.rb` are
 # run as spec files by default. This means that files in spec/support that end
